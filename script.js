4cca5edc956fd882620785b6523dceda95cb0519
Devyn Bailey <devynbailey11@yahoo.com>
2020-10-31 15:09:00 -0600

    updated css and html, fixed js to display api data

--- script.js
+++ script.js
@@ -7,37 +7,47 @@
 //6. make card/div/element for future 5 day weather cast
 // -- future 5 day card needs to show: displays the date, an icon representation of weather conditions, the temperature, and the humidity
 
-
 //declaring variables
 var searchButton = document.getElementById("search-button");
-var apiKey = '1d11cd9fa7c01b9a2502e818bcb69115' //my openweatherapi key
-var cityName = "Denver"
+var apiKey = "1d11cd9fa7c01b9a2502e818bcb69115"; //my openweatherapi key
+var cityName = "Denver";
 
 //button event listener that calls the openweatherapi function
 searchButton.addEventListener("click", getCityWeather);
 
 //function to get openweatherapi data
-function getCityWeather() {
-    //maybe not right?
-    // var searchInput = document.getElementById("search-input").value
-    // console.log(searchInput);
-    var currentDisplay = document.getElementById('currentDisplay');
-    //fetching openweatherapi data
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey)
-        .then(response => response.json())
-        //call to function
-        .then(data => currentDataDisplay(data))
+function getCityWeather(event) {
+  event.preventDefault();
+  //maybe not right?
+  // var searchInput = document.getElementById("search-input").value
+  // console.log(searchInput);
 
-    function currentDataDisplay(data) {
-        //calls certiain values from data
-        const {name, main, weather, wind,} = data;
+  //fetching openweatherapi data
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=" +
+      apiKey
+  )
+    .then((response) => response.json())
+    //call to function
+    .then((data) => currentDataDisplay(data));
 
-        document.getElementById('name').textContent = name;
-        document.getElementById('wind').textContent = wind
+  function currentDataDisplay(data) {
+    var cityName = data.name;
+    var cityHumidity = data.main.humidity;
+    var cityWind = data.wind.speed;
+    var cityTemp = data.main.temp;
 
-        // console.log(name);
-        // console.log(main);
-    } 
-        
+    var stateName = document.getElementById("name");
+    var humidity = document.getElementById("humidity");
+    var wind = document.getElementById("wind");
+    var temp = document.getElementById("temp");
+
+    stateName.innerText = cityName;
+    humidity.innerText = "Humidity: " + cityHumidity;
+    wind.innerText = "Wind Speed: " + cityWind;
+    temp.innerText = "Current Temperature: " + cityTemp;
+  }
 }
-console.log(searchButton);
+// console.log(searchButton);
