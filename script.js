f7653117287469ccace1328e283750b97b20045a
Devyn Bailey <devynbailey11@yahoo.com>
2020-11-01 20:48:58 -0700

    got js to show previous searched cities

--- script.js
+++ script.js
@@ -6,13 +6,19 @@
 //5. for UV index: presented with a color that indicates whether the conditions are favorable, moderate, or severe
 //6. make card/div/element for future 5 day weather cast
 // -- future 5 day card needs to show: displays the date, an icon representation of weather conditions, the temperature, and the humidity
+//need to set up local storage and userInput
 
 //declaring variables
 var searchButton = document.getElementById("search-button");
+// var userInput = document.getElementById("")
 var apiKey = "1d11cd9fa7c01b9a2502e818bcb69115"; //my openweatherapi key
-var cityName = "Denver";
-var cityLat = 39.74;
-var cityLon = -104.98;
+
+// var cityName = document.getElementById("name");
+var cityName;
+var cityLat;
+var cityLon;
+
+var previousCities = [];
 
 //button event listener that calls the openweatherapi function
 searchButton.addEventListener("click", getCityWeather);
@@ -20,12 +26,17 @@ searchButton.addEventListener("click", getCityWeather);
 //function to get openweatherapi data
 function getCityWeather(event) {
   event.preventDefault();
-  //maybe not right?
-  // var searchInput = document.getElementById("search-input").value
-  // console.log(searchInput);
 
-  //fetching openweatherapi data
-  //add &units=imperial
+  cityName = document.getElementById("search-input").value;
+  console.log(cityName);
+
+  localStorage.setItem("previous" + previousCities.length, cityName);
+
+  previousCities.push("previous" + previousCities.length + "");
+  console.log(previousCities[0]);
+  updatePreviousCities();
+
+  //fetching openweatherapi data for current weather
   fetch(
     "http://api.openweathermap.org/data/2.5/weather?q=" +
       cityName +
@@ -34,40 +45,68 @@ function getCityWeather(event) {
   )
     .then((response) => response.json())
     //call to function
-    .then((data) => currentDataDisplay(data));
-      return fetch("http://api.openweathermap.org/data/2.5/uvi?q" +
-      "&lat=" + cityLat + "&lon=" + cityLon +
-      "&appid=" +
-      apiKey)
-
-      .then((response) => response.json())
-      //call to function
-      .then((data) => currentUVDisplay(data));
+    .then((data) => currentDataDisplay(data))
+    .catch((error) => {
+      throw new Error("City Doesnt Exist");
+    });
 
   function currentDataDisplay(data) {
+    //declaring variables to display data
     var cityName = data.name;
     var cityHumidity = data.main.humidity;
     var cityWind = data.wind.speed;
     var cityTemp = data.main.temp;
-    // var cityUV = data.value;
 
+    //UV index fetch
+    fetch(
+      "http://api.openweathermap.org/data/2.5/uvi?q" +
+        "&lat=" +
+        data.coord.lat +
+        "&lon=" +
+        data.coord.lon +
+        "&appid=" +
+        apiKey
+    )
+      .then((response) => response.json())
+      //call to function
+      .then((data) => currentUVDisplay(data));
+
+    //declaring variablese that call the html IDs
     var stateName = document.getElementById("name");
     var humidity = document.getElementById("humidity");
     var wind = document.getElementById("wind");
     var temp = document.getElementById("temp");
-    // var uv = document.getElementById("uv");
 
+    //putting api data into webpage
     stateName.innerText = cityName;
     humidity.innerText = "Humidity: " + cityHumidity;
     wind.innerText = "Wind Speed: " + cityWind;
     temp.innerText = "Current Temperature: " + cityTemp;
-    // uv.innerText = "UV Index: " + cityUV;
   }
 
-  function currentUVDisplay (data) {
+  function currentUVDisplay(data) {
     var cityUV = data.value;
     var uv = document.getElementById("uv");
     uv.innerText = "UV Index: " + cityUV;
   }
+
+  function updatePreviousCities () {
+
+    var cityDiv = document.getElementById('previousCities');
+    
+    //clearing children out 
+    while (cityDiv.firstChild) {
+      cityDiv.removeChild(cityDiv.firstChild);
+    }
+
+    for(i = 0; i < previousCities.length; i++) {
+      var prevCity = localStorage.getItem(previousCities[i]);
+      var prevCityDiv = document.createElement('div');
+
+      prevCityDiv.textContent = prevCity;
+      cityDiv.appendChild(prevCityDiv);
+    }
+  }
+
 }
 // console.log(searchButton);
